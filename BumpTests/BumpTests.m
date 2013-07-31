adee1188a7d3f879257c5653b96c911447619fa4
Aleks Kamko <aykamko@gmail.com>
2013-07-30 18:16:19 -0700

    Initial Commit

---
+++ BumpTests/BumpTests.m
@@ -0,0 +1,36 @@
+//
+//  BumpTests.m
+//  BumpTests
+//
+//  Created by Aleks Kamko on 7/30/13.
+//  Copyright (c) 2013 Facebook. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BumpTests : XCTestCase
+
+@end
+
+@implementation BumpTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
